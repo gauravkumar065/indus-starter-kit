05b535b6650cc576570a3f7d3094f5c55cc92856
gaurav <gauravkmr065@gmail.com>
2024-11-12 19:25:17 +0530

    fix

--- components/dashboard/nav-user.tsx
+++ components/dashboard/nav-user.tsx
@@ -25,7 +25,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { useClerk, UserButton } from "@clerk/nextjs";
+import { useClerk } from "@clerk/nextjs";
 import Link from "next/link";
 export function NavUser({
   user,
